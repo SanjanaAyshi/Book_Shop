bb07854de07942beb4ce1f5ed12f26d59f3f3eae
Sanjana Ayshi <sanjana.ayshi92@gmail.com>
2024-01-18 00:00:54 +0600

    csrf token add

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    'https://book-shop-o6f8.onrender.com', 'https://*.127.0.0.1']
+    'https://hotelplaza.onrender.com/', 'https://*.127.0.0.1']
 
 # Application definition
 
